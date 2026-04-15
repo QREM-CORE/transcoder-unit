0a2bf2f5c9af54794dde73a96a1e8fbc2f2cd315
Kiet Le <ktlbusiness11@gmail.com>
2026-04-15 16:19:46 -0400

    Add modules to rtl.f

--- rtl.f
+++ rtl.f
@@ -1,5 +1,9 @@
 # 1. Submodules (i.e. -f lib/keccak-fips202-sv/rtl.f)
 
 # 2. Local Packages (i.e., rtl/my_pkg.sv)
+rtl/transcoder_pkg.sv
 
 # 3. Local RTL (i.e., rtl/transcoder_unit.sv)
+rtl/compress.sv
+rtl/decompress.sv
+
