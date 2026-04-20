c40cc10bb0e448390d7cb99ad77ac00edfaa8d90
Kiet Le <ktlbusiness11@gmail.com>
2026-04-20 16:40:25 -0400

    Try to fix yosys build

--- rtl/transcoder_pkg.sv
+++ rtl/transcoder_pkg.sv
@@ -1,6 +1,12 @@
 package transcoder_pkg;
 
-    import qrem_global_pkg::*;
-    export qrem_global_pkg::*;
+    // Explicit import + export
+    import qrem_global_pkg::tr_opcode_t;
+    import qrem_global_pkg::COEFF_WIDTH;
+    import qrem_global_pkg::Q;
+
+    export qrem_global_pkg::tr_opcode_t;
+    export qrem_global_pkg::COEFF_WIDTH;
+    export qrem_global_pkg::Q;
 
 endpackage : transcoder_pkg
